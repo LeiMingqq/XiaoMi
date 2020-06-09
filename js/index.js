57b7d8fe5ddef72ecf2df485b477c41ce5403462
leimingqq <1494381296@qq.com>
2020-06-09 16:51:41 +0800

    js有点不够好，html还有一些图片没有更改。

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
+// 上部分隐藏的轮播图
+function testSct() {
+    var navUl = document.querySelector(".header-nav ul"),
+        roRotation = document.querySelector(".container-rotation"),
+        contImg = document.querySelector(".container-rotation .container ul");
+    index = 0;//表示第几页
+
+    // 切换图片的方法
+    function switchImg(n) {
+        var value = n * -100 + "%";
+        contImg.style.marginLeft = value;
+    }
+    // 移入相应的属性 跟着切换
+    navUl.onmouseover = function (e) {
+        var children = Array.from(navUl.children);
+        var value = children.indexOf(e.target);
+        if (value < 0 || value > 7) {
+            // clearTimeout(item,900);
+            if (value != -1) {
+                roRotation.style.height = "0px";
+                var item = setTimeout(function () {
+                    roRotation.style.display = "none";
+                }, 1000);
+            }
+            return false;
+        }
+        // 鼠标移入变成块盒
+        if (value >= 0 && value <= 7) {
+            roRotation.style.display = "block";
+            var item = setTimeout(function () {
+                clearInterval(item);
+                roRotation.style.height = "229px";
+            }, 200);
+        }
+        switchImg(value - 1);
+    }
+}
+
 // 控制大轮播图
 function test() {
     // 控制大轮播图
@@ -100,105 +138,250 @@ function test() {
 }
 
 // 小米闪购区域
-function testwot(){
+function testwot() {
     // 获取倒计时的类
-var round = document.querySelector(".round"),
-span = document.querySelector(".countdown");
-// 小轮播图
-var rotation = document.querySelector(".rt-rotation ul"),
-rtButton = document.querySelector(".box-rotation .rt-button"),
-index = 0;//表示第几页
-clebe = "";
+    var round = document.querySelector(".round"),
+        span = document.querySelector(".countdown");
+    // 小轮播图
+    var rotation = document.querySelector(".rt-rotation ul"),
+        rtButton = document.querySelector(".box-rotation .rt-button"),
+        index = 0;//表示第几页
+    clebe = "";
 
-//设一个方法模拟倒计时
-function countDown() {
-round.innerHTML = "22:00 场";
-var s = 22 - (new Date().getHours());//时
-var f = 60 - (new Date().getMinutes());//分
-var m = 60 - (new Date().getSeconds());//秒
-var item = setInterval(function () {
-    span.children[4].innerHTML = m;
-    m--;
-    if (m < 0) {
-        m = 59;
-        f--;
-        if (f < 0) {
-            f = 59;
-            if (s > 0) {
-                s--;
+    //设一个方法模拟倒计时
+    function countDown() {
+        round.innerHTML = "22:00 场";
+        var s = 22 - (new Date().getHours());//时
+        var f = 60 - (new Date().getMinutes());//分
+        var m = 60 - (new Date().getSeconds());//秒
+        var item = setInterval(function () {
+            span.children[4].innerHTML = m;
+            m--;
+            if (m < 0) {
+                m = 59;
+                f--;
+                if (f < 0) {
+                    f = 59;
+                    if (s > 0) {
+                        s--;
+                    }
+                }
             }
+            if (s == 0 && f == 0 && s == 0) {
+                clearInterval(item)
+                alert("该睡觉了")
+            }
+            span.children[0].innerHTML = s;
+            span.children[2].innerHTML = f;
+        }, 1000);
+    }
+
+    // 设计一个方法控制小轮播图
+    function roTion(n) {
+        var value = n * -100 + "%";
+        rotation.style.marginLeft = value;
+    }
+
+    // 这个方法是向左切换
+    rtButton.children[0].onclick = function () {
+        if (index <= 4) {
+            rtButton.children[1].style.color = "#b0b0b0";
+            rtButton.children[1].style.cursor = "pointer";
+        }
+        if (index == 1) {
+            rtButton.children[0].style.color = "#e0e0e0";
+            rtButton.children[0].style.cursor = "default";
+            index--;
+            roTion(index);
+            return false;
+        }
+        if (index >= 1) {
+            index--;
+            roTion(index);
         }
     }
-    if (s == 0 && f == 0 && s == 0) {
-        clearInterval(item)
-        alert("该睡觉了")
+    // 鼠标移入颜色发生改变
+    rtButton.children[0].onmouseover = function () {
+        if (index != 0) {
+            stop();
+            rtButton.children[0].style.color = "#ff6700";
+        }
     }
-    span.children[0].innerHTML = s;
-    span.children[2].innerHTML = f;
-}, 1000);
+    // 鼠标移出颜色变回
+    rtButton.children[0].onmouseout = function () {
+        if (index != 0) {
+            rtButton.children[0].style.color = "#b0b0b0";
+        }
+        setTimeout(start, 5000);
+    }
+    // 这个方法是向右切换
+    rtButton.children[1].onclick = function () {
+        if (index == 0) {
+            rtButton.children[0].style.color = "#b0b0b0";
+            rtButton.children[0].style.cursor = "pointer";
+        }
+        if (index == 3) {
+            rtButton.children[1].style.color = "#e0e0e0";
+            rtButton.children[1].style.cursor = "default";
+            index++;
+            roTion(index);
+            return false;
+        }
+        if (index <= 3) {
+            index++;
+            roTion(index);
+        }
+    }
+    // 鼠标移入颜色发生改变
+    rtButton.children[1].onmouseover = function () {
+        if (index != 4) {
+            stop();
+            rtButton.children[1].style.color = "#ff6700";
+        }
+    }
+    // 鼠标移出颜色变回
+    rtButton.children[1].onmouseout = function () {
+        if (index != 4) {
+            rtButton.children[1].style.color = "#b0b0b0";
+        }
+    }
+    // 设置一个定时器 规定 它多少秒切换一次
+    function start() {
+        clearInterval(clebe);
+        clebe = setInterval(function () {
+            index++;
+            if (index == 5) {
+                index = 0;
+            }
+            roTion(index);
+        }, 8000);
+    }
+    start();
+    // 停止切换
+    function stop() {
+        clearInterval(clebe);
+    }
+    countDown();
 }
 
-// 设计一个方法控制小轮播图
-function roTion(n) {
-var value = n * -100 + "%";
-rotation.style.marginLeft = value;
-}
+// 图片标签控制切换区域
+function testShi() {
+    var tabList = document.getElementsByClassName("tab-list");
+    var bdRight = document.getElementsByClassName("bd-right");
 
-// 这个方法是向左切换
-rtButton.children[0].onclick = function () {
-if (index <= 4) {
-    rtButton.children[1].style.color = "#b0b0b0";
-    rtButton.children[1].style.cursor = "pointer";
-}
-if (index == 1) {
-    rtButton.children[0].style.color = "#e0e0e0";
-    rtButton.children[0].style.cursor = "default";
-    index--;
-    roTion(index);
-    return false;
-}
-if (index >= 1) {
-    index--;
-    roTion(index);
-}
-}
-// 这个方法是向右切换
-rtButton.children[1].onclick = function () {
-if (index == 0) {
-    rtButton.children[0].style.color = "#b0b0b0";
-    rtButton.children[0].style.cursor = "pointer";
-}
-if (index == 3) {
-    rtButton.children[1].style.color = "#e0e0e0";
-    rtButton.children[1].style.cursor = "default";
-    index++;
-    roTion(index);
-    return false;
-}
-if (index <= 3) {
-    index++;
-    roTion(index);
-}
-}
-// 设置一个定时器 规定 它多少秒切换一次
-function start() {
-clearInterval(clebe);
-clebe = setInterval(function () {
-    index++;
-    if (index == 5) {
-        index = 0;
+    // 切换标签 1
+    function testTitle(n) {
+        var active = document.getElementsByClassName("tab-active");
+        active[0].className = "";
+        tabList[0].children[n].className = "tab-active";
+        // 判断 是0展示1 是1展示2   
+        if (n == 1) {
+            bdRight[1].children[n].style.zIndex = "1";
+        } else {
+            bdRight[1].children[1].style.zIndex = "-1";
+        }
     }
-    roTion(index);
-}, 8000);
-}
-start();
-// 停止切换
-function stop() {
-clearInterval(clebe);
-}
-countDown();
+    tabList[0].onmouseover = function (e) {
+        var children = Array.from(tabList[0].children);
+        var value = children.indexOf(e.target);
+        if (value == -1) {
+            return false;
+        }
+        // 调用切换方法 传值 切换
+        testTitle(value);
+    }
+
+    // 切换标签 2
+    function testTitle1(n) {
+        var active = document.getElementsByClassName("tab-active");
+        active[1].className = "";
+        tabList[1].children[n].className = "tab-active";
+        if (n == 1) {
+            bdRight[2].children[n].style.zIndex = "1";
+            bdRight[2].children[2].style.zIndex = "-1";//每次鼠标移入第二个，清除 第3个
+        } else if (n == 2) {
+            bdRight[2].children[n].style.zIndex = "2";
+        } else {
+            bdRight[2].children[1].style.zIndex = "-1";
+            bdRight[2].children[2].style.zIndex = "-1";
+        }
+    }
+    tabList[1].onmouseover = function (e) {
+        var children = Array.from(tabList[1].children);
+        var value = children.indexOf(e.target);
+        if (value == -1) {
+            return false;
+        }
+        testTitle1(value);
+    }
+
+    // 切换标签 3
+    function testTitle2(n) {
+        var active = document.getElementsByClassName("tab-active");
+        active[2].className = "";
+        tabList[2].children[n].className = "tab-active";
+        if (n == 1) {
+            bdRight[3].children[n].style.zIndex = "1";
+        } else {
+            bdRight[3].children[1].style.zIndex = "-1";
+        }
+    }
+    tabList[2].onmouseover = function (e) {
+        var children = Array.from(tabList[2].children);
+        var value = children.indexOf(e.target);
+        if (value == -1) {
+            return false;
+        }
+        testTitle2(value);
+    }
+
+    // 切换标签 4
+    function testTitle3(n) {
+        var active = document.getElementsByClassName("tab-active");
+        active[3].className = "";
+        tabList[3].children[n].className = "tab-active";
+        if (n == 1) {
+            bdRight[4].children[n].style.zIndex = "1";
+            bdRight[4].children[2].style.zIndex = "-1";//每次鼠标移入第二个，清除 3
+        } else if (n == 2) {
+            bdRight[4].children[n].style.zIndex = "2";
+        } else {
+            bdRight[4].children[1].style.zIndex = "-1";
+            bdRight[4].children[2].style.zIndex = "-1";
+        }
+    }
+    tabList[3].onmouseover = function (e) {
+        var children = Array.from(tabList[3].children);
+        var value = children.indexOf(e.target);
+        if (value == -1) {
+            return false;
+        }
+        testTitle3(value);
+    }
+
+    // 切换标签 5
+    function testTitle4(n) {
+        var active = document.getElementsByClassName("tab-active");
+        active[4].className = "";
+        tabList[4].children[n].className = "tab-active";
+        if (n == 1) {
+            bdRight[5].children[n].style.zIndex = "1";
+        } else {
+            bdRight[5].children[1].style.zIndex = "-1";
+        }
+    }
+    tabList[4].onmouseover = function (e) {
+        var children = Array.from(tabList[4].children);
+        var value = children.indexOf(e.target);
+        if (value == -1) {
+            return false;
+        }
+        testTitle4(value);
+    }
 }
 
 // 执行方法
 test();
 testwot();
+testSct();
+testShi();
